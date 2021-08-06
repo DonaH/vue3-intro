1e15eb1acc8a8cb6001fd386b193e9848bf795d4
donah <dona.hsieh@gmail.com>
2021-08-05 22:07:38 -0700

    Add image and url bindings

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'V-logo super soft socks'
+      image: './assets/images/socks_green.jpg',
+      url: 'https://donah.github.io'
     }
   }
 })
